925bf3768c06a5963fc11b39e5796c54e116ef89
leoshimo <56844000+leoshimo@users.noreply.github.com>
2026-09-08 20:37:27 -0700

    Update ditooctl to 0.1.2

--- Formula/ditooctl.rb
+++ Formula/ditooctl.rb
@@ -1,8 +1,8 @@
 class Ditooctl < Formula
   desc "Direct remote for the Divoom Ditoo pixel display"
   homepage "https://github.com/leoshimo/ditooctl"
-  url "https://github.com/leoshimo/ditooctl/releases/download/v0.1.1/ditooctl-0.1.1-macos-universal.tar.gz"
-  sha256 "a5ec8a7631f659120149d50655f35e7c36b61506a9e16194be751bb5585fb58a"
+  url "https://github.com/leoshimo/ditooctl/releases/download/v0.1.2/ditooctl-0.1.2-macos-universal.tar.gz"
+  sha256 "134d748d94d738db53f3a88d783d6f2483e08ea5a24e91bb1cc670df3a04a6b8"
   license "MIT"
   depends_on :macos => :ventura
 
@@ -11,6 +11,6 @@ class Ditooctl < Formula
   end
 
   test do
-    assert_equal "0.1.1", shell_output("#{bin}/ditooctl --version").strip
+    assert_equal "0.1.2", shell_output("#{bin}/ditooctl --version").strip
   end
 end
